2a0a6016f1adc120c2edf95fb8abdf5b29feb823
friday-james <lamcollin37@gmail.com>
2023-11-22 19:28:46 -0800

    Check Transfer on Humanode

--- Humanode/Humanode-starter/src/mappings/mappingHandlers.ts
+++ Humanode/Humanode-starter/src/mappings/mappingHandlers.ts
@@ -1,7 +1,8 @@
 import { Codec } from "@polkadot/types/types";
 import { Vec } from "@polkadot/types-codec";
 import { SubstrateEvent } from "@subql/types";
-import { BioauthNewAuthentication, ImOnlineSomeOffline } from "../types";
+import { BioauthNewAuthentication, ImOnlineSomeOffline, Transfer } from "../types";
+import { Balance } from "@polkadot/types/interfaces";
 
 export async function handleBioauthNewAuthenticationEvent(
   substrateEvent: SubstrateEvent
@@ -45,4 +46,25 @@ export async function handleImonlineSomeOfflineEvent(
     record.accountIds.push(accountId.toString());
   }
   await record.save();
+}
+
+export async function handleEvent(event: SubstrateEvent): Promise<void> {
+  // Get data from the event
+  // The balances.transfer event has the following payload \[from, to, value\]
+  // logger.info(JSON.stringify(event));
+  const from = event.event.data[0];
+  const to = event.event.data[1];
+  const amount = event.event.data[2];
+
+  // Create the new transfer entity
+  const transfer = new Transfer(
+    `${event.block.block.header.number.toNumber()}-${event.idx}`,
+    from.toString(),
+    to.toString(),
+  );
+  transfer.blockNumber = event.block.block.header.number.toBigInt();
+  transfer.from = from.toString();
+  transfer.to = to.toString();
+  transfer.amount = (amount as Balance).toBigInt();
+  await transfer.save();
 }
